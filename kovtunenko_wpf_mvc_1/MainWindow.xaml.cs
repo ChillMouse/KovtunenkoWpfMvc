6fbf8a0367b1dae4699cc272648ea9b6d4096533
brendonn <rorkopxrnrxv@mail.ru>
2022-03-04 19:56:58 +0700

    Добавьте файлы проекта.

---
+++ kovtunenko_wpf_mvc_1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kovtunenko_wpf_mvc_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            View.MenuSystem menuSystem = new View.MenuSystem();
+            menuSystem.Show();
+        }
+    }
+
+}
