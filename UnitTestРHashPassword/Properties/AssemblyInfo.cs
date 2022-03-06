3ed1b92cdeb40cb173dceb1b133f95dd78f16e74
brendonn <rorkopxrnrxv@mail.ru>
2022-03-06 18:27:45 +0700

    develop: создание проекста UNIT-теста, настройка и отладка. Переименовние unitTestHashFunction -> ManualUnitTestHashFunction; Завершение проведения работ. Выкатываем в master.

---
+++ UnitTestРHashPassword/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestHashPassword")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestHashPassword")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7b4d7a4-3ef5-44a6-83c6-1091fc117bf0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
